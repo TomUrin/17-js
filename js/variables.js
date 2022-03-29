ccea9bef231e6c87a8aafc3b0118b312926d8a21
TomasUri <urinovic.tomas@gmail.com>
2022-03-29 13:55:07 +0300

    Variables

---
+++ js/variables.js
@@ -0,0 +1,27 @@
+const one = 1;
+console.log(one);
+const two = 2;
+console.log(two);
+const three = 3;
+console.log(three);
+
+const first = 'one';
+console.log(first);
+const second = 'two';
+console.log(second);
+const third = 'three';
+console.log(third);
+
+const numbers = [1, 2, 3, 4, 5];
+console.log(numbers);
+const cats = [5, 4, 3, 2, 1];
+console.log(cats);
+const dogs = [2, 1, 4, 3, 5];
+console.log(dogs);
+
+const names = ['John', 'Tom', 'Nick', 'Norah', 'Annie'];
+console.log(names);
+const cars = ['Audi', 'BMW', 'Mercedes', 'Toyota', 'Tesla'];
+console.log(cars);
+const brands = ['Microsoft', 'Apple', 'Sony', 'Xiaomi', 'Samsung'];
+console.log(brands);
